4807064149411f82737a2089d15a9032757c2f86
dimirob <dimitriosrvosnakis@gmail.com>
2021-04-27 17:50:10 +0300

    Update Services.java
    Δημιουργία τελικών κλάσεων για: Υπηρεσίες, Υπηρεσίες ιντερνετ, Υπηρεσίες Τηλεφώνου.

--- Services.java
+++ Services.java
@@ -5,4 +5,33 @@ class Services{
         this.serviceFee=fee;
         this.serviceName=name;
     }
+    public String getserviceName(){
+        return this.serviceName;
+    }
+    public float getserviceFee(){
+        return this.serviceFee;
+    }
+}
+class PhoneServices extends Services{
+    private int freeMinutes;
+    private int freeSMS;
+    private float minutesCost;
+    private float smsCost;
+    public PhoneServices(String name,float fee,int mins,int sms,float minCost,float smsCost){
+        super(name, fee);
+        this.freeMinutes=mins;
+        this.freeSMS=sms;
+        this.minutesCost=minCost;
+        this.smsCost=smsCost;
+    }
+}
+class DataServices extends Services{
+    private final float discount=(float)0.3;
+    private int freeData;
+    private float dataCost;
+    public DataServices(String name,float fee,int freeData,float dataCost){
+        super(name, fee);
+        this.dataCost=dataCost;
+        this.freeData=freeData;
+    }
 }
