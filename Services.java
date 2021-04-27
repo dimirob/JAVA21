fb887638dd1a602190586a7bfec397eb2b36ff89
dimirob <dimitriosrvosnakis@gmail.com>
2021-04-27 17:18:36 +0300

    Update Services.java

---
+++ Services.java
@@ -0,0 +1,8 @@
+class Services{
+    private String serviceName;
+    private float serviceFee;
+    public Services(String name,float fee){
+        this.serviceFee=fee;
+        this.serviceName=name;
+    }
+}
