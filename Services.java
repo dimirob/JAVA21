b16ffee94516d05f0291d4d65366ec72f31b9277
dimirob <dimitriosrvosnakis@gmail.com>
2021-04-28 10:42:32 +0300

    Update Services.java
    Προσθήκη σχολίων και κλάσης καρτοσυμβολαίου.

--- Services.java
+++ Services.java
@@ -1,16 +1,16 @@
 class Services{
-    private String serviceName;
-    private float serviceFee;
+    private String serviceName; //onoma yphresias
+    private float serviceFee; //pagio yphresias
     public Services(String name,float fee){
         this.serviceFee=fee;
         this.serviceName=name;
     }
 }
 class PhoneServices extends Services{
-    private int freeMinutes;
-    private int freeSMS;
-    private float minutesCost;
-    private float smsCost;
+    private int freeMinutes; //dwrean lepta
+    private int freeSMS; //dwrean sms
+    private float minutesCost; //kostos leptwn meta apo dwrean
+    private float smsCost; //kostos sms meta apo dwrean 
     public PhoneServices(String name,float fee,int mins,int sms,float minCost,float smsCost){
         super(name, fee);
         this.freeMinutes=mins;
@@ -21,11 +21,18 @@ class PhoneServices extends Services{
 }
 class DataServices extends Services{
     private final float discount=(float)0.3; //Ypothetoume ekptwsh pou dinetai sthn ekfwnhsh
-    private int freeData;
-    private float dataCost;
+    private int freeData; //dwrean dedomena
+    private float dataCost; //kostos dedomenwn meta apo dwrean
     public DataServices(String name,float fee,int freeData,float dataCost){
         super(name, fee);
         this.dataCost=dataCost;
         this.freeData=freeData;
+    }
+}
+class Card extends PhoneServices{
+    float budget; //arxiko mhniaio upoloipo gia ta kartosumvolaia
+    public Card(String name,float fee,int mins,int sms,float minCost,float smsCost,float startingBudget){
+        super(name,fee,mins,sms,minCost,smsCost);
+        this.budget=startingBudget;
     }
 }
